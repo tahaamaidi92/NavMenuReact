f08f2aa91e538effc97a1d067b90f1b8bdc36dda
tahaamaidi92 <benhama.taha.33@gmail.com>
2019-05-13 14:57:39 +0100

    Updates

---
+++ precache-manifest.b2a41d8fccae4754fb16aaf6e54dd181.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6edcd7da031c3e859d673e4226c6cc28",
+    "url": "/NavMenuReact/index.html"
+  },
+  {
+    "revision": "c1e3ade8b51aafe6c960",
+    "url": "/NavMenuReact/static/css/main.2f50c52d.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/NavMenuReact/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "c1e3ade8b51aafe6c960",
+    "url": "/NavMenuReact/static/js/main.0e10c371.chunk.js"
+  },
+  {
+    "revision": "05cf3563a2702f8761f0",
+    "url": "/NavMenuReact/static/js/runtime~main.6fd7c075.js"
+  }
+]);
